9f15262cf03e230e57c2e0f37b93706c8cd689e9
Alex2 <aleksander1802@gmail.com>
2023-05-24 23:24:00 +0300

    feat: added config for react, added types

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,10 +3,23 @@ import path from "path";
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader"
+    ]
+  }
+
   const typeScriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [typeScriptLoader];
+  return [
+    typeScriptLoader, 
+    cssLoader
+  ];
 }
