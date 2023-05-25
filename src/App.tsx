2bd73f1e7ee5bfa72eba0f905dbce3f6a428f062
Alex2 <aleksander1802@gmail.com>
2023-05-25 18:34:57 +0300

    feat: added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
-import AboutPage from '../pages/AboutPage/AboutPage';
-import MainPage from '../pages/MainPage/MainPage';
+import './styles/index.scss';
+
 import { Link } from 'react-router-dom';
-import { MainPageAsync } from '../pages/MainPage/MainPage.async';
-import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async';
-import { Suspense } from 'react';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense, useContext, useState } from 'react';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={() => toggleTheme()}>TOGGLE</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
