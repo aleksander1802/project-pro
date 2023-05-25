6fdf623a41848bf623460ef7796e9cf34fa1671f
Alex2 <aleksander1802@gmail.com>
2023-05-26 00:06:58 +0300

    feat: added widget navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,16 @@ import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 
 import './styles/index.scss';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={() => toggleTheme()}>TOGGLE</button>
-      <Link to="/">Главная</Link>
-      <Link to="/about">О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={() => toggleTheme()}>TOGGLE</button>
     </div>
   );
 };
