f6a55a990f71723f05952b374e587779ba7956a7
Alex2 <aleksander1802@gmail.com>
2023-05-26 22:43:01 +0300

    feat: added loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,16 +1,23 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { RouteConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(RouteConfig).map(({ element, path }) => (
-          <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {Object.values(RouteConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <div className="page-wrapper">{element}</div>
+            </Suspense>
+          }
+        />
+      ))}
+    </Routes>
   );
 };
 
