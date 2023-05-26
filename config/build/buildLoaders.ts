6056e6a1381d46cc0cc291ab306db2dba3797b1f
Alex2 <aleksander1802@gmail.com>
2023-05-26 14:13:43 +0300

    feat: added svg/file loader, added custom button component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,35 +1,46 @@
-import webpack from "webpack";
+import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from "./types/config";
+import { BuildOptions } from './types/config';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
 
-  const {isDev} = options
+  const { isDev } = options;
 
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           modules: {
-            auto: (resPath:string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]'
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : '[hash:base64:8]',
           },
-        }
+        },
       },
-      "sass-loader"
-    ]
-  }
+      'sass-loader',
+    ],
+  };
 
   const typeScriptLoader = {
     test: /\.tsx?$/,
-    use: "ts-loader",
+    use: 'ts-loader',
     exclude: /node_modules/,
   };
-  return [
-    typeScriptLoader, 
-    cssLoader
-  ];
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+
+  return [fileLoader, svgLoader, typeScriptLoader, cssLoader];
 }
