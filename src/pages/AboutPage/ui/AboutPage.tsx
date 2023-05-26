0ee4ff94286d7f771e23e09e10a4a4b6c610df3e
Alex2 <aleksander1802@gmail.com>
2023-05-26 19:54:09 +0300

    feat: added stylelint

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
-    const { t } = useTranslation('about');
-    return <div>{t('about')}</div>;
+  const { t } = useTranslation('about');
+  return <div>{t('about')}</div>;
 };
 
 export default AboutPage;
