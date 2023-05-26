fc3054a93a328dfbb029ae6246d91700338052c3
Alex2 <aleksander1802@gmail.com>
2023-05-26 18:05:09 +0300

    feat: added babel, added extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,26 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
   const { isDev } = options;
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -42,5 +62,5 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoader, typeScriptLoader, cssLoader];
+  return [fileLoader, svgLoader, babelLoader, typeScriptLoader, cssLoader];
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
