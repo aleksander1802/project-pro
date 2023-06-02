2ad33b3bd97d224e6af941085efa0e590939d4b5
Alex2 <aleksander1802@gmail.com>
2023-06-02 13:39:28 +0300

    feat: added loginmodal, added loginform

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,4 @@
-import { Counter } from 'entities/Counter';
-import React from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import Modal from 'shared/ui/Modal/Modal';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useCallback, useState } from 'react';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,8 +15,12 @@ export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false);
+  }, []);
+
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true);
   }, []);
 
   return (
@@ -23,15 +28,11 @@ export const Navbar = ({ className }: NavbarProps) => {
       <Button
         theme={ButtonTheme.CLEAR_INVERTED}
         className={cls.links}
-        onClick={onToggleModal}
+        onClick={onShowModal}
       >
         {t('enter')}
       </Button>
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        {t(
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptatem eius, at ad, voluptatibus doloremque uam explicabo, ab ullam repellendus perspiciatis veniam?',
-        )}
-      </Modal>
+      <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
     </div>
   );
 };
