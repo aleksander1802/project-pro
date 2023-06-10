ad4ede1de54532eeaab9ace67005930a5b628dae
Alex2 <aleksander1802@gmail.com>
2023-06-10 18:46:08 +0300

    feat: added fetching articles, added types switcher

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,46 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import { ArticleView } from 'entities/Article/model/types/article';
+import ListIcon from 'shared/assets/icons/list-24-24.svg';
+import TileIcon from 'shared/assets/icons/tile-24-24.svg';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import cls from './ArticleViewSelector.module.scss';
+
+interface ArticleViewSelectorProps {
+  className?: string;
+  view: ArticleView;
+  onViewClick?: (view: ArticleView) => void;
+}
+
+const viewTypes = [
+  {
+    view: ArticleView.TILE,
+    icon: TileIcon,
+  },
+  {
+    view: ArticleView.LIST,
+    icon: ListIcon,
+  },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+  const { className, view, onViewClick } = props;
+
+  const onClick = (newView: ArticleView) => () => {
+    onViewClick?.(newView);
+  };
+
+  return (
+    <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+      {viewTypes.map((v) => (
+        <Button theme={ButtonTheme.CLEAR} onClick={onClick(v.view)}>
+          <Icon
+            Svg={v.icon}
+            className={classNames('', { [cls.selected]: v.view !== view })}
+          />
+        </Button>
+      ))}
+    </div>
+  );
+});
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,23 +1,64 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { Article, ArticleList } from 'entities/Article';
+import { memo, useCallback } from 'react';
+import { ArticleList, ArticleViewSelector } from 'entities/Article';
 import { ArticleView } from 'entities/Article/model/types/article';
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import {
+  getArticlesPageError,
+  getArticlesPageIsLoading,
+  getArticlesPageView,
+} from '../../model/selectors/articlesPageSelector';
+import {
+  articlesPageAction,
+  articlesPageReducer,
+  getArticles,
+} from '../../model/slices/articlesPageSlice';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
   className?: string;
 }
 
+const reducers: ReducersList = {
+  articlesPage: articlesPageReducer,
+};
+
 const ArticlesPage = (props: ArticlesPageProps) => {
   const { className } = props;
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const articles = useSelector(getArticles.selectAll);
+  const isLoading = useSelector(getArticlesPageIsLoading);
+  const view = useSelector(getArticlesPageView);
+  const error = useSelector(getArticlesPageError);
+
+  const onChangeView = useCallback(
+    (view: ArticleView) => {
+      dispatch(articlesPageAction.setView(view));
+    },
+    [dispatch],
+  );
+
+  useInitialEffect(() => {
+    dispatch(fetchArticlesList());
+    dispatch(articlesPageAction.initState());
+  });
 
   return (
-    <div className={classNames(cls.ArticlesPage, {}, [className])}>
-      {' '}
-      <ArticleList isLoading view={ArticleView.LIST} articles={[]} />
-    </div>
+    <DynamicModuleLoader reducers={reducers}>
+      <div className={classNames(cls.ArticlesPage, {}, [className])}>
+        <ArticleViewSelector view={view} onViewClick={onChangeView} />
+        <ArticleList isLoading={isLoading} view={view} articles={articles} />
+      </div>
+    </DynamicModuleLoader>
   );
 };
 
