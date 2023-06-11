d722d2b1a0eb5d7b3b304764c1731b622fd05b77
Alex2 <aleksander1802@gmail.com>
2023-06-11 15:36:05 +0300

    feat: added ui state, scroll restore

--- src/app/providers/storeProvider/config/StateSchema.ts
+++ src/app/providers/storeProvider/config/StateSchema.ts
@@ -16,10 +16,12 @@ import { ArticleDetailsSchema } from 'entities/Article';
 import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
 import { AddCommentFormSchema } from 'features/addCommentForm';
 import { ArticlesPageSchema } from 'pages/ArticlesPage';
+import { UISchema } from 'features/UI';
 
 export interface StateSchema {
   counter: CounterSchema;
   user: UserSchema;
+  ui: UISchema;
 
   loginForm?: LoginSchema;
   profile?: ProfileSchema;
@@ -30,7 +32,7 @@ export interface StateSchema {
 }
 
 export type StateSchemaKey = keyof StateSchema;
-export type MountedReducers = OptionalRecord<StateSchemaKey, boolean>
+export type MountedReducers = OptionalRecord<StateSchemaKey, boolean>;
 
 export interface ReducerManager {
   getReducerMap: () => ReducersMapObject<StateSchema>;
