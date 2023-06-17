578d748243669d3fe9bd242260780882ac5f4117
Alex2 <aleksander1802@gmail.com>
2023-06-17 17:16:48 +0300

    feat: added dropdown

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -7,9 +7,94 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: 100 }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof ListBox>;
 
-const Template: ComponentStory<typeof ListBox> = () => <ListBox />;
+const Template: ComponentStory<typeof ListBox> = (args) => (
+  <ListBox {...args} />
+);
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+  value: '4124',
+  items: [
+    {
+      content: '123',
+      value: 't4n45ym45',
+    },
+    {
+      content: '12512551',
+      value: 'bn4643737',
+    },
+  ],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+  direction: 'top left',
+  value: '4124',
+  items: [
+    {
+      content: '123',
+      value: 't4n45ym45',
+    },
+    {
+      content: '12512551',
+      value: 'bn4643737',
+    },
+  ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+  direction: 'top right',
+  value: '4124',
+  items: [
+    {
+      content: '123',
+      value: 't4n45ym45',
+    },
+    {
+      content: '12512551',
+      value: 'bn4643737',
+    },
+  ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+  direction: 'bottom left',
+  value: '4124',
+  items: [
+    {
+      content: '123',
+      value: 't4n45ym45',
+    },
+    {
+      content: '12512551',
+      value: 'bn4643737',
+    },
+  ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+  direction: 'bottom right',
+  value: '4124',
+  items: [
+    {
+      content: '123',
+      value: 't4n45ym45',
+    },
+    {
+      content: '12512551',
+      value: 'bn4643737',
+    },
+  ],
+};
