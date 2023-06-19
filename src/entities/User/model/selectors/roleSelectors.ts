daac3ccd3d08cfa8964c41d7b60bba3601fee36f
Alex2 <aleksander1802@gmail.com>
2023-06-19 18:13:03 +0300

    feat: added custom babel plugin, data-testid remover

--- src/entities/User/model/selectors/roleSelectors.ts
+++ src/entities/User/model/selectors/roleSelectors.ts
@@ -1,12 +1,10 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
-import { UserRole } from 'entities/User';
+import { UserRole } from '../consts/consts';
 
 export const getUserRoles = (state: StateSchema) => state.user.authData?.roles;
 
 export const isUserAdmin = createSelector(getUserRoles, (roles) =>
-  Boolean(roles?.includes(UserRole.ADMIN)),
-);
+  Boolean(roles?.includes(UserRole.ADMIN)));
 export const isUserManager = createSelector(getUserRoles, (roles) =>
-  Boolean(roles?.includes(UserRole.MANAGER)),
-);
+  Boolean(roles?.includes(UserRole.MANAGER)));
