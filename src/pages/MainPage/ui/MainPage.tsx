6708a417c6ad26289800f9c470ffab42a5d102ff
Alex2 <aleksander1802@gmail.com>
2023-07-17 15:00:23 +0300

    feat: added star rating

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,20 @@
 import { useTranslation } from 'react-i18next';
 import Page from '@/widgets/Page/Page';
+import { RatingCard } from '@/entities/Rating/index';
 
 const MainPage = () => {
   const { t } = useTranslation();
 
-  return <Page>{t('Главная страница')}</Page>;
+  return (
+    <Page>
+      {t('Главная страница')}
+      <RatingCard
+        title={t('articleFeedback')}
+        feedbackTitle={t('articleFeedbackTitle')}
+        hasFeedback
+      />
+    </Page>
+  );
 };
 
 export default MainPage;
