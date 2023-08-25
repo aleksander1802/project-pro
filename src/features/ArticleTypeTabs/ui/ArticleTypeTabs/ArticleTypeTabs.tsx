60b4ee3ab3da9484e105912cc36a135064da2bb6
Alex2 <aleksander1802@gmail.com>
2023-08-25 14:35:48 +0300

    feat: refactored entities

---
+++ src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
@@ -0,0 +1,54 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback, useMemo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import Tabs, { TabItem } from '@/shared/ui/Tabs/Tabs';
+import { ArticleType } from '@/entities/Article';
+
+interface ArticleTypeTabsProps {
+  className?: string;
+  value: ArticleType;
+  onChangeType: (type: ArticleType) => void;
+}
+
+export const ArticleTypeTabs = memo((props: ArticleTypeTabsProps) => {
+  const { className, value, onChangeType } = props;
+  const { t } = useTranslation();
+
+  const typeTabs = useMemo<TabItem[]>(
+    () => [
+      {
+        value: ArticleType.ALL,
+        content: t('allArticleType'),
+      },
+      {
+        value: ArticleType.IT,
+        content: t('ITArticleType'),
+      },
+      {
+        value: ArticleType.ECONOMICS,
+        content: t('economicArticleType'),
+      },
+      {
+        value: ArticleType.SCIENCE,
+        content: t('scienceArticleType'),
+      },
+    ],
+    [t],
+  );
+
+  const onTabClick = useCallback(
+    (tab: TabItem) => {
+      onChangeType(tab.value as ArticleType);
+    },
+    [onChangeType],
+  );
+
+  return (
+    <Tabs
+      tabs={typeTabs}
+      value={value}
+      onTabClick={onTabClick}
+      className={classNames('', {}, [className])}
+    />
+  );
+});
--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -2,14 +2,7 @@ import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import {
-  ArticleSortField,
-  ArticleSortSelector,
-  ArticleType,
-  ArticleTypeTabs,
-  ArticleView,
-  ArticleViewSelector,
-} from '@/entities/Article';
+import { ArticleSortField, ArticleType, ArticleView } from '@/entities/Article';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import Card from '@/shared/ui/Card/Card';
 import { Input } from '@/shared/ui/Input/Input';
@@ -26,6 +19,9 @@ import {
 } from '../../model/selectors/articlesPageSelector';
 import { articlesPageActions } from '../../model/slices/articlesPageSlice';
 import cls from './ArticlesPageFilters.module.scss';
+import { ArticleSortSelector } from '@/features/ArticleSortSelector';
+import { ArticleViewSelector } from '@/features/ArticleViewSelector';
+import { ArticleTypeTabs } from '@/features/ArticleTypeTabs';
 
 interface ArticlesPageFiltersProps {
   className?: string;
